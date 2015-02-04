7c504952b313e4343d34c2f637abc9b0d9f88c61
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-04 17:43:01 -0500

    Demo action bubbling (controller, nested route, route, app route)

---
+++ app/routes/application.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save: function() {
+      console.log('+---- save bubbled up to application route');
+      return true;
+    },
+    cancel: function() {
+      console.log('+---- cancel bubbled up to application route');
+      return true;
+    }
+  }
+});
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
+  },
+
+  actions: {
+    save: function() {
+      console.log('+-- save bubbled up to friends/new route');
+      return true;
+    },
+    cancel: function() {
+      console.log('+-- cancel bubbled up to friends/new route');
+      return true;
+    }
   }
 });
