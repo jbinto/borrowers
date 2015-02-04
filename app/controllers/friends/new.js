43d817f6dfa50f9acf1cab40b9db97a7b24b8e44
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-04 17:39:53 -0500

    Add new user form, up to but not including persistence

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    save: function() {
+      console.log('+- save called from friends new controller');
+      return true;
+    },
+    cancel: function() {
+      console.log('+- cancel called from friends new controller');
+      return true;
+    }
+  }
 });
