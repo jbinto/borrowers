0da88b8253e9354422c5f4b6125e7c644a745930
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-11 15:33:48 -0500

    Implement "add friend", with naive validation

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,14 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.firstName',
+    'model.lastName',
+    'model.email',
+    'model.twitter',
+    function() {
+      return Ember.isPresent(this.get('model.firstName')) &&
+        Ember.isPresent(this.get('model.lastName')) &&
+        Ember.isPresent(this.get('model.email')) &&
+        Ember.isPresent(this.get('model.twitter'));
+    }
+  ),
   actions: {
     save: function() {
-      console.log('+- save called from friends new controller');
-      return true;
+      if (this.get('isValid')) {
+        var self = this;
+        this.get('model').save().then(function(friend) {
+          self.transitionToRoute('friends.show', friend);
+        });
+      }
+      else {
+        this.set('errorMessage', 'Please complete the entire form.');
+      }
+
+      return false;
     },
     cancel: function() {
-      console.log('+- cancel called from friends new controller');
-      return true;
+      this.transitionToRoute('friends');
+      return false;
     }
   }
 });
