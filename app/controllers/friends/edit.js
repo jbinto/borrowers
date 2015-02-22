258999b86ca73e708b69bd12cd40047349a520e1
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-22 15:28:19 -0500

    Refactor friends new/edit controllers into base controller

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -1,33 +1,9 @@
-import Ember from 'ember';
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.firstName',
-    'model.lastName',
-    'model.email',
-    'model.twitter',
-    function() {
-      return Ember.isPresent(this.get('model.firstName')) &&
-        Ember.isPresent(this.get('model.lastName')) &&
-        Ember.isPresent(this.get('model.email')) &&
-        Ember.isPresent(this.get('model.twitter'));
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save: function() {
-      if (this.get('isValid')) {
-        var self = this;
-        this.get('model').save().then(function(friend) {
-          self.transitionToRoute('friends.show', friend);
-        });
-      }
-      else {
-        this.set('errorMessage', 'Please complete the entire form.');
-      }
-
-      return false;
-    },
     cancel: function() {
+      // n.b. from within a route, this would be simply `transitionTo`
       this.transitionToRoute('friends.show', this.get('model'));
       return false;
     }
--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,32 +1,7 @@
-import Ember from 'ember';
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.firstName',
-    'model.lastName',
-    'model.email',
-    'model.twitter',
-    function() {
-      return Ember.isPresent(this.get('model.firstName')) &&
-        Ember.isPresent(this.get('model.lastName')) &&
-        Ember.isPresent(this.get('model.email')) &&
-        Ember.isPresent(this.get('model.twitter'));
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save: function() {
-      if (this.get('isValid')) {
-        var self = this;
-        this.get('model').save().then(function(friend) {
-          self.transitionToRoute('friends.show', friend);
-        });
-      }
-      else {
-        this.set('errorMessage', 'Please complete the entire form.');
-      }
-
-      return false;
-    },
     cancel: function() {
       this.transitionToRoute('friends');
       return false;
