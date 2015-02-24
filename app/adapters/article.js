0f6c5544ca9df14b713e4561a0c7725b0ddedecb
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-24 15:05:51 -0500

    Get rid of ArticleAdapter, never meant to be implemented!
    https://github.com/abuiles/ember-cli-101-errata/issues/172

--- app/adapters/article.js
+++
@@ -1,8 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default ApplicationAdapter.extend({
-  buildURL: function(type, id , record) {
-    console.log("buildURL(type,id,record) called: " + type + " " + id + " " + record);
-    return '/friends/' + record.get('friend.id') + '/articles/' + id;
-  }
-});
