5ebb7e3978c2b088f0ec5a29282a102dbd69b6d6
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-24 13:40:50 -0500

    Fix article adapter (inherit from this app's ApplicationAdapter)

--- app/adapters/article.js
+++ app/adapters/article.js
@@ -1,7 +1,8 @@
-import {ApplicationAdapter} from 'ember';
+import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   buildURL: function(type, id , record) {
+    console.log("buildURL(type,id,record) called: " + type + " " + id + " " + record);
     return '/friends/' + record.get('friend.id') + '/articles/' + id;
   }
 });
