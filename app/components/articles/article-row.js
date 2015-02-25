32e87cf1b9fdf0d59c806f3929f9646dcbb46385
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-25 18:05:49 -0500

    Auto update when borrowed article's status changes

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,9 +4,18 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null,          // passed in
   articleStates: null,    // passed in
-  actions: {
-    saveArticle: function(article) {
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+  stateChanged: function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      // sort of like setTimeout(..., 0):
+      //  "run this as soon as possible, but not sooner"
+      Ember.run.once(this, this.autoSave);
+    }
+  }.on('init').observes('article.state')
 });
