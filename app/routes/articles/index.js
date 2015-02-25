b43c5561dcd3ddc973a90467394f7f14ee2cd489
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-25 16:33:52 -0500

    Implement {{article-row}} component [...]
    components/articles/article-row.js: Defines the component's tag name, properties and (deferred) actions.
    components/articles/article-row.hbs: Defines the actual inner content of the component (a bunch of <td>s).
    routes/article/index.js: Handle save(article) action.
    controllers/article/index.js: Define `possibleStates` enum.
    templates/articles/index.hbs: Component is dropped in here.

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.modelFor('friends/show').get('articles');
+  },
+  actions: {
+    save: function(article) {
+      console.log("article.index route save called");
+      article.save();
+      return false;
+    }
   }
 });
