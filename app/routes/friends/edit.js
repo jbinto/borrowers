ae638337bd236fd9e3b240998f60dc4abc96786e
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-25 19:28:35 -0500

    Fix issue where cancelling "Edit Friend" doesn't clean up after itself

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  deactivate: function() {
+    console.log('friend.edit route deactivated');
+    var model = this.modelFor('friends/edit');
+    model.rollback();
+  }
 });
