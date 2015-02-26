81784a583c7e3e4abea90c4e310677a8d7303a05
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-25 19:32:23 -0500

    Add moment to Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -25,4 +25,6 @@ app.import('vendor/fontello/font/fontello.eot', { destDir: 'font' });
 app.import('vendor/fontello/font/fontello.svg', { destDir: 'font' });
 app.import('vendor/fontello/font/fontello.woff', { destDir: 'font' });
 
+app.import('bower_components/moment/moment.js');
+
 module.exports = app.toTree();
