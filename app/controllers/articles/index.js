c97ae39ec71ea37a37b9a923d49a61065312938e
Jesse Buchanan <hello@jessebuchanan.ca>
2015-03-02 16:33:06 -0500

    Demonstrate observing model.@each and model.[] on Articles Index

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  possibleStates: ['borrowed', 'returned']
+  possibleStates: ['borrowed', 'returned'],
+
+  contentDidChange: function() {
+    console.log('The article collection was changed.');
+  }.observes('model.[]'),
+  stateDidChange: function() {
+    console.log('One of the articles states was changed.');
+  }.observes('model.@each.state')
 });
